a7df9eff50fd27273b2ba4b4d32b5642a89cca70
Alfath <furqan.alfath@emerson.com>
2021-06-28 22:08:58 +0700

    Learn SASS

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+//var postcss = require('gulp-postcss');
+var paths = {
+    styles: {
+        src: 'css/**/*.scss',
+        dest: 'css'
+    }
+}
+function scss() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+exports.default = scss
